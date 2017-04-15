a257edb328ab5c9335ac57e5c9f416207aaab98e
WangZhaomeng <wangzhaomeng@wangzhaomengdemacbook-pro.local>
2017-04-15 17:51:04 +0800

    Initial Commit

---
+++ LLVideoPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LLVideoPlayer
+//
+//  Created by WangZhaomeng on 2017/4/15.
+//  Copyright © 2017年 MaoChao Network Co. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LLVideoPlayer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LLVideoPlayer
+//
+//  Created by WangZhaomeng on 2017/4/15.
+//  Copyright © 2017年 MaoChao Network Co. Ltd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
